ad30174fadff2d11280415f292f7a37ae4d0ae54
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2024-01-21 16:58:14 +0600

    Refactor App component and add content to Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Profile from "./Pages/Profile";
+import SignIn from "./Pages/SignIn";
+import SignUp from "./Pages/SignUp";
 
-const App = () => {
+export default function App() {
   return (
-    <div>
-      <h1>React App</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/about" element={<Profile />} />
+        <Route path="/about" element={<SignIn />} />
+        <Route path="/about" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   );
-};
-
-export default App;
+}
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>Home page</p>
+    </div>
+  );
 };
 
 export default Home;
